3ae768f73eb516a98f479cbdac08bf88033a9934
cortez-hugo <h.art.cor24@gmail.com>
2025-05-19 18:45:26 -0400

    initial setup

---
+++ src/main/java/net/gnarleygamestudios/cobblemonservermod/CobblemonServerModClient.java
@@ -0,0 +1,10 @@
+package net.gnarleygamestudios.cobblemonservermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CobblemonServerModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
---
+++ src/main/java/net/gnarleygamestudios/cobblemonservermod/CobblemonServerModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.gnarleygamestudios.cobblemonservermod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CobblemonServerModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
